84a578b001dd26fcf2e99978430645b1e577332c
alishahid2565 <alishahid.cyberx@gmail.com>
2024-12-17 08:27:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,7 +19,7 @@ module.exports = {
 
 ᴄʏʙᴇʀ x  ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ`,
   ALIVE_MEDIA: 'https://telegra.ph/file/d842677f522b6bef4dfe6.jpg',
-  MONGODB_URL: 'mongodb+srv://hs03254424439:<4mdXTSvi71kykYU2>@cluster0.axkn0.mongodb.net/', 
+  MONGODB_URL: 'mongodb+srv://hs03254424439:<db_password>@cluster0.axkn0.mongodb.net/', 
   PREFIX: '.',
   LANGUAGE: 'EN',
   PORT: 3000,
